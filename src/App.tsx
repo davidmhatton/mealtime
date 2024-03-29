5e62bd96729112ad20923b7877160600f7096cdb
David Hatton <david.m.hatton@gmail.com>
2024-03-29 22:11:26 +0000

    style(app): custom theming

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import WeekAhead from "./pages/WeekAhead";
 import Planner from "./pages/Planner";
+import { mealtimeTheme } from "./theme";
+import { ThemeProvider } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <TopBar />
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/planner"} element={<Planner />} />
-          <Route path={"/week-ahead"} element={<WeekAhead />} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider theme={mealtimeTheme}>
+      <div className="App">
+        <Router>
+          <TopBar />
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/planner"} element={<Planner />} />
+            <Route path={"/week-ahead"} element={<WeekAhead />} />
+          </Routes>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const mealtimeTheme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "3rem",
+    },
+    h2: {
+      fontSize: "2rem",
+    },
+  },
+});
